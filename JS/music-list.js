bc2f9f149d7a19e8a072bfe709ab1489a9c18af8
boztran <boztran125k@gmail.com>
2023-01-12 21:55:00 +0700

    first commit

---
+++ JS/music-list.js
@@ -0,0 +1,67 @@
+let allMusic = [{
+        name: "Anh Làm Tất Cả Vì",
+        artist: "TRẦN GIA BẢO",
+        img: "1",
+        src: "1"
+    },
+    {
+        name: "Vương Vấn",
+        artist: "TRẦN GIA BẢO",
+        img: "2",
+        src: "2"
+    },
+    {
+        name: "Lời Có Cánh",
+        artist: "TRẦN GIA BẢO",
+        img: "3",
+        src: "3"
+    },
+    {
+        name: "Million Dollar Boy",
+        artist: "TRẦN GIA BẢO",
+        img: "4",
+        src: "4"
+    },
+    {
+        name: "Mập Mờ Là Dại Khờ",
+        artist: "TRẦN GIA BẢO",
+        img: "5",
+        src: "5"
+    },
+    {
+        name: "Hạ Cánh",
+        artist: "TRẦN GIA BẢO",
+        img: "6",
+        src: "6"
+    },
+    {
+        name: "Chỉ Bằng Cái Gật Đầu",
+        artist: "TRẦN GIA BẢO",
+        img: "7",
+        src: "7"
+    },
+    {
+        name: "Đại Diện Cho Trái Tim",
+        artist: "TRẦN GIA BẢO",
+        img: "8",
+        src: "8"
+    },
+    {
+        name: "Cứu Vãn Kịp Không",
+        artist: "TRẦN GIA BẢO",
+        img: "9",
+        src: "9"
+    },
+    {
+        name: "Muốn Bên Anh Thật Không",
+        artist: "TRẦN GIA BẢO",
+        img: "10",
+        src: "10"
+    },
+    {
+        name: "Mah Boo",
+        artist: "TRẦN GIA BẢO",
+        img: "11",
+        src: "11"
+    },
+];
